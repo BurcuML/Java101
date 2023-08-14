d87993314c98baf3ad4d205ce8994108e0c8fc96
Burcu M. Leblebici <56592520+BurcuML@users.noreply.github.com>
2023-08-14 18:19:15 +0300

    HesapMakinesi added

---
+++ src/HesapMakinesi/Main.java
@@ -0,0 +1,40 @@
+package HesapMakinesi;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("İlk sayıyı giriniz:");
+        double num1 = scanner.nextDouble();
+        System.out.println("İkinci sayıyı giriniz:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Yapmak istediğiniz işlem için numara seçiniz: ");
+        System.out.println("1:Toplama" + " " + "2: Çıkarma"+  " " +"3:Bölme" + " " + "4: Çarpma");
+        int choice = scanner.nextInt();
+        double result;
+        switch (choice){
+            case 1:
+                result = (num1 + num2);
+                System.out.println("Sonuç: " + result);
+            break;
+            case 2:
+                result = (num1 - num2);
+                System.out.println("Sonuç: " + result);
+                break;
+            case 3:
+                result = (num1 / num2);
+                System.out.println("Sonuç: " + result);
+                break;
+            case 4:
+                result = (num1 * num2);
+                System.out.println("Sonuç: " + result);
+                break;
+            default:
+                System.out.println("Yanlış seçim yaptınız. Lütfen tekrar deneyiniz.");
+        }
+
+    }
+}
